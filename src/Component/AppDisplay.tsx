3283fae5de3e5ef58050289f1fd43bbc45f731a0
lexuanhuy <Lexuanhuy8695@gmail.com>
2023-10-20 15:13:53 +0700

    update pagination

--- src/Component/AppDisplay.tsx
+++ src/Component/AppDisplay.tsx
@@ -5,14 +5,14 @@ import { Input } from 'antd';
 import { Button } from 'antd';
 import Content from "./Content"
 import axios from "axios";
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Stargazers from "./Stargazers"
 const AppDisplay = () => {
     const [owner, setOwner] = useState("")
     const [repo, setRepo] = useState("")
     const [data, setData] = useState("")
-
-    const [dataDetail, setDataDetail] = useState<object>([])
+    const [page, setPage] = useState(1)
+    const [dataDetail, setDataDetail] = useState([])
 
     const handleShowDataGit = async () => {
 
@@ -40,7 +40,7 @@ const AppDisplay = () => {
 
     const handleGetAllStarUser = async (owner: string, repo: string) => {
         try {
-            let data = await axios.get(`https://api.github.com/repos/${owner}/${repo}/stargazers`,
+            let data = await axios.get(`https://api.github.com/repos/${owner}/${repo}/stargazers?page=${page}&per_page=${12}`,
                 {
                     headers: {
                         Authorization: 'Bearer ' + import.meta.env.VITE_TOKEN,
@@ -49,18 +49,27 @@ const AppDisplay = () => {
                 })
             if (data.status == import.meta.env.VITE_SUCCESS_CODE) {
                 setDataDetail(data?.data)
-
+                if (page > 1 && data?.data?.length == 0) {
+                    setPage(page - 1)
+                    await axios.get(`https://api.github.com/repos/${owner}/${repo}/stargazers?page=${page}&per_page=${10}`,
+                        {
+                            headers: {
+                                Authorization: 'Bearer ' + import.meta.env.VITE_TOKEN,
+                                "Content-Type": "application/json"
+                            }
+                        })
+                }
             }
-
         } catch (error: any) {
             if (error?.response?.status == import.meta.env.VITE_NOTFOUND_CODE) {
                 alert("Not found")
             }
         }
     }
+    useEffect(() => {
 
-
-
+        handleGetAllStarUser(owner, repo)
+    }, [page])
     return (
         <div className="AppDisplay_container">
             <div className="wrapper">
@@ -108,6 +117,22 @@ const AppDisplay = () => {
                     />
                 </div>
             </div>
+            {dataDetail && dataDetail.length > 0 &&
+                <div className='load-more'>
+                    {page > 1 &&
+                        <Button type="primary" onClick={() => setPage(page - 1)}>Back</Button>
+
+                    }
+
+
+                    <Button type="primary" onClick={() => setPage(page + 1)}>Load more</Button>
+
+
+
+                </div>
+            }
+
+
         </div>
     )
 
